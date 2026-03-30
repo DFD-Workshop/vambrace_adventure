e0dba4e64d4a2e538f6b3479b1b2850d99b990e4
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2026-03-30 17:50:10 -0500

    Episode 6: Changing topics of arms after mini type error from cmd to command

--- vambrace_micro_ros_app/lib/vambrace_config/vambrace_config.hpp
+++ vambrace_micro_ros_app/lib/vambrace_config/vambrace_config.hpp
@@ -2,8 +2,8 @@
 
 // Topics
 #define TOPIC_CMD_VEL "/mobile_base_controller/cmd_vel"
-#define TOPIC_LEFT_ARM "/simple_left_arm_controller/cmd"
-#define TOPIC_RIGHT_ARM "/simple_right_arm_controller/cmd"
+#define TOPIC_LEFT_ARM "/simple_left_arm_controller/commands"
+#define TOPIC_RIGHT_ARM "/simple_right_arm_controller/commands"
 #define TOPIC_EMOTION "/emotion/int"
 #define TOPIC_TTS "/orion_response"
 
